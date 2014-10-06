c0b25210c80ec7dd1f3e4960566ab4e28b8f2c56
semccoy <semccoy@vassar.edu>
2014-10-05 20:58:33 -0400

    world array working, drawing functions working
    got rid of null pointer errors

--- game1/src/game1/Game1.java
+++ game1/src/game1/Game1.java
@@ -28,6 +28,7 @@ import tester.*;
 import javalib.funworld.*;
 import javalib.worldimages.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Game1 extends World {
@@ -38,26 +39,21 @@ public class Game1 extends World {
     static int OFFSET = 8;
     static int BACKWIDTH = (OFFSET - 2) * WIDTH / OFFSET;
     static int BACKHEIGHT = (OFFSET - 2) * HEIGHT / OFFSET;
-    static Posn base = new Posn(WIDTH / 2, HEIGHT / 2);
-    static Posn upperleft = new Posn(200, 120);
-    static Posn lowerright = new Posn(1240, 680);
     static int CELLSIZE = 40;
     static int CELLSWIDE = BACKWIDTH / CELLSIZE;
     static int CELLSHIGH = BACKHEIGHT / CELLSIZE;
+    static Posn base = new Posn(WIDTH / 2, HEIGHT / 2);
+    static Posn upperleft = new Posn(200, 120);
+    static Posn lowerright = new Posn(1240, 680);
 
     public static WorldImage universe = new RectangleImage(base, WIDTH, HEIGHT, Color.black);
     public static WorldImage background = new RectangleImage(base, BACKWIDTH, BACKHEIGHT, Color.lightGray);
-    
-    
-    public Square[] worldArray;
-    // all drawing functions should just add new Squares to worldArray
-    // makeImage should just iterate through and return everything to draw the world
-    
-    
 
-    public WorldImage drawField(WorldImage background) {
+    // this is everything
+    private static ArrayList<RectangleImage> worldArray = new ArrayList<RectangleImage>();
+
+    public void addBackground(ArrayList<RectangleImage> worldArray) {
         Color color;
-        WorldImage newscene = background;
         for (int x = 0; x < CELLSWIDE; x++) {
             for (int y = 0; y < CELLSHIGH; y++) {
                 int XSTART = upperleft.x + x * CELLSIZE;
@@ -65,51 +61,50 @@ public class Game1 extends World {
                 if (x == 0 || y == 0 || x == CELLSWIDE - 1 || y == CELLSHIGH - 1) {
                     color = Color.red;
                 } else {
-                    color = Color.white;
+                    color = Color.lightGray;
                 }
-                newscene = new OverlayImages(newscene, new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, color));
+                worldArray.add(new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, color));
             }
         }
-        return newscene;
     }
 
-    // use this when someone beats a level
-    // also increase game speed for a few seconds for maximum celebratory effect
-    public WorldImage drawFieldWin(WorldImage background) {
-        WorldImage newscene = background;
-        for (int x = 0; x < CELLSWIDE; x++) {
-            for (int y = 0; y < CELLSHIGH; y++) {
-                int XSTART = upperleft.x + x * CELLSIZE;
-                int YSTART = upperleft.y + y * CELLSIZE;
-                newscene = new OverlayImages(newscene, new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, randomColor()));
-            }
-        }
-        return newscene;
-    }
-
-    public WorldImage drawRock(int x, int y, WorldImage background) {
+    public void addRock(ArrayList<RectangleImage> worldArray, int x, int y) {
         Color color = Color.gray;
-        WorldImage newscene = background;
         int XSTART = upperleft.x + (x % (CELLSWIDE - 2) + 1) * CELLSIZE;
         int YSTART = upperleft.y + (y % (CELLSHIGH - 2) + 1) * CELLSIZE;
-        newscene = new OverlayImages(newscene, new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, color));
+        worldArray.add(new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, color));
+    }
+
+    public void addGoal(ArrayList<RectangleImage> worldArray, int x, int y) {
+        Color color = Color.cyan;
+        int XSTART = upperleft.x + (x % (CELLSWIDE - 2) + 1) * CELLSIZE;
+        int YSTART = upperleft.y + (y % (CELLSHIGH - 2) + 1) * CELLSIZE;
+        worldArray.add(new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, color));
+    }
+
+    public ArrayList<RectangleImage> allTheSmallThings() {
+        addBackground(worldArray);
+        addRock(worldArray, 5, 7);
+        addRock(worldArray, 15, 7);
+        addGoal(worldArray, 10, 7);
+        worldArray.trimToSize();
+        return worldArray;
+    }
+
+    public WorldImage buildWorld(ArrayList<RectangleImage> worldArray) {
+        WorldImage newscene = universe;
+        for (int i = 0; i < worldArray.size(); i++) {
+            RectangleImage temp = worldArray.get(i);
+            newscene = new OverlayImages(newscene, temp);
+        }
         return newscene;
     }
 
-    public WorldImage drawCharacter(WorldImage background) {
+    public WorldImage addCharacter(WorldImage background) {
         Color color = Color.green;
         WorldImage newscene = background;
         int XSTART = upperleft.x + CELLSIZE;
         int YSTART = upperleft.y + CELLSIZE;
-        newscene = new OverlayImages(newscene, new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, color));
-        return newscene;
-    }
-
-    public WorldImage drawGoal(WorldImage background) {
-        Color color = Color.cyan;
-        WorldImage newscene = background;
-        int XSTART = lowerright.x - CELLSIZE;
-        int YSTART = lowerright.y - CELLSIZE;
         newscene = new OverlayImages(newscene, new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, color));
         return newscene;
     }
@@ -124,6 +119,20 @@ public class Game1 extends World {
         return new Color(randomInt(0, 255), randomInt(0, 255), randomInt(0, 255));
     }
 
+    // use this when someone beats a level
+    // also increase game speed for a few seconds for maximum celebratory effect
+    public WorldImage addWin(WorldImage background) {
+        WorldImage newscene = background;
+        for (int x = 0; x < CELLSWIDE; x++) {
+            for (int y = 0; y < CELLSHIGH; y++) {
+                int XSTART = upperleft.x + x * CELLSIZE;
+                int YSTART = upperleft.y + y * CELLSIZE;
+                newscene = new OverlayImages(newscene, new RectangleImage(new Posn(XSTART, YSTART), CELLSIZE, CELLSIZE, randomColor()));
+            }
+        }
+        return newscene;
+    }
+
     public Game1(WorldImage uni) {
         super();
         this.universe = uni;
@@ -134,7 +143,8 @@ public class Game1 extends World {
     }
 
     public WorldImage makeImage() {
-        return new OverlayImages(this.universe, drawCharacter(drawGoal(drawRock(6, 0, drawField(background)))));
+        allTheSmallThings();
+        return buildWorld(worldArray);
     }
 
     public static void main(String[] args) {
@@ -145,19 +155,19 @@ public class Game1 extends World {
 }
 
 /*
-A field of play where the blocks move.
+ A field of play where the blocks move.
 
-A set of "live" blocks that are player controlled. (This "set" may contain one block.)
+ A set of "live" blocks that are player controlled. (This "set" may contain one block.)
 
-A set of "dead" blocks that are no longer player controlled.
+ A set of "dead" blocks that are no longer player controlled.
 
-A scoring system.
+ A scoring system.
 
-A win or fail state.
+ A win or fail state.
 
-A control mechanism.
+ A control mechanism.
 
-You should write a short game manual that describes the rules of your game. You should run this past me so we can agree that the game is complex and interesting enough. You should use the invariants of your game to design testable components. 
+ You should write a short game manual that describes the rules of your game. You should run this past me so we can agree that the game is complex and interesting enough. You should use the invariants of your game to design testable components. 
 
-You should be able to build a completely automated version of your game for testing. For example, in my Tetris game, I might parameterize the game over a Tetrimino generator and a Input stream, so that I can test explicit sequences of inputs on Block sequences and ensure that the rules of the game are enforced.
-*/
+ You should be able to build a completely automated version of your game for testing. For example, in my Tetris game, I might parameterize the game over a Tetrimino generator and a Input stream, so that I can test explicit sequences of inputs on Block sequences and ensure that the rules of the game are enforced.
+ */
